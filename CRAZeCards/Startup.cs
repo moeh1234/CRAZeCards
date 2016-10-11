d4a809fc6c8dc5d0030704b3ab85b88a82cf3e35
moeh1234 <moeh1234@gmail.com>
2016-10-11 16:28:41 -0400

    Add project files.

---
+++ CRAZeCards/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CRAZeCards.Startup))]
+namespace CRAZeCards
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
