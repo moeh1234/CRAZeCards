1774b6957c04fe01e2b3733a0c690431823bf672
moeh1234 <moeh1234@gmail.com>
2016-10-12 13:33:07 -0400

    Team eCoders
    Layout done for index.

---
+++ CRAZeCards/Controllers/CRAZeCardsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CRAZeCards.Controllers
+{
+    public class CRAZeCardsController : Controller
+    {
+        // GET: CRAZeCards
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
