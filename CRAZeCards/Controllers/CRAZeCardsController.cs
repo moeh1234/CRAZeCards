9f5c27b60358e558cfbaa33bde97ec790848e3f2
moeh1234 <moeh1234@gmail.com>
2016-10-14 14:22:43 -0400

    MailGun Messaging WORKING!

--- CRAZeCards/Controllers/CRAZeCardsController.cs
+++ CRAZeCards/Controllers/CRAZeCardsController.cs
@@ -24,18 +24,10 @@ namespace CRAZeCards.Controllers
 
             return View(jan);
         }
-<<<<<<< HEAD
-        public ActionResult ViewGodDay()
-        {
-            return View();
-        }
-=======
->>>>>>> origin/master
         [HttpGet]
         public ActionResult SendCard(string holidayId, string imgUrl)
         {
             HolidayEntity entity = new HolidayEntity();
-<<<<<<< HEAD
            HOLIDAY holiday = entity.HOLIDAYS.Single(x => x.Holiday_Id == holidayId);
 
             return View(holiday);
@@ -44,33 +36,26 @@ namespace CRAZeCards.Controllers
         public ActionResult SendCard(FormCollection form)
         {
             string toEMAIL = form["toemail"];
-            SendSimpleMessage(toEMAIL);
+            IRestResponse response = SendSimpleMessage(toEMAIL);
             return View("MessageSent");
         }
-
-
         public static IRestResponse SendSimpleMessage(string email)
         {
             RestClient client = new RestClient();
             client.BaseUrl = new Uri("https://api.mailgun.net/v3");
             client.Authenticator =
                     new HttpBasicAuthenticator("api",
-                                               "key-5a66becd274a49eba1921a2545080bb1");
+                                               "key-a13e230e7ba7be810d04ebaba1b72574");
             RestRequest request = new RestRequest();
             request.AddParameter("domain",
-                                 "YOUR_DOMAIN_NAME", ParameterType.UrlSegment);
+                                 "sandbox72fcdacea882400d8b44f1952838a66e.mailgun.org", ParameterType.UrlSegment);
             request.Resource = "{domain}/messages";
-            request.AddParameter("from", "_Name_ <donotreply@carzyeCards.org>");
-            request.AddParameter("to", email);
+            request.AddParameter("from", "_Name_ <excited@sandbox72fcdacea882400d8b44f1952838a66e.mailgun.org>");
+            request.AddParameter("to", "ajrashid92@gmail.com");
             request.AddParameter("subject", "Hello");
             request.AddParameter("text", "Testing some Mailgun awesomness!");
             request.Method = Method.POST;
             return client.Execute(request);
         }
-=======
-            HOLIDAY holiday = entity.HOLIDAYS.Single(x => x.Holiday_Id == holidayId);
-            return View(holiday);
-        }
->>>>>>> origin/master
     }
 }
